84621421284d3928e4331d1951f9b45f1d88b556
Bruno Badu <brunobadu9@gmail.com>
2023-11-03 19:36:03 -0300

    adicionando operadores matematicos

---
+++ aula1/comentarios.py
@@ -0,0 +1,15 @@
+# Comentário em apenas uma linha simples
+
+'''
+Este é um comentário
+que pode ter mais de uma linha
+e nada será exibido no console
+'''
+
+"""
+Este
+aqui
+também
+não será
+exibido
+"""
---
+++ aula1/operadores_matematicos.py
@@ -0,0 +1,14 @@
+# Soma
+print('1 + 1 =', 1+1)
+# Para concatenar em python, use ','
+
+# Subtração
+print('1-1 =', 1-1)
+
+# Multiplicação
+
+print('7x8 =', 7*8)
+
+# Divisão
+
+print('30 / 5 =', 30 / 5)
